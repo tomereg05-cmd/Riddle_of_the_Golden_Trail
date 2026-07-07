b541079a4da9be4ad2922685a738a178ddf06d70
tomereg05-cmd <tomereg05@gmail.com>
2026-07-07 10:36:02 +0300

    adjusting docker for a local database

--- backend/prisma/migrations/20260707065502_init/migration.sql
+++ backend/prisma/migrations/20260707065502_init/migration.sql
@@ -2,20 +2,19 @@
 CREATE TABLE "Aircraft" (
     "id" SERIAL NOT NULL,
     "icao24" TEXT NOT NULL,
-    "callsign" TEXT NOT NULL,
+    "callsign" TEXT,
     "origin_country" TEXT NOT NULL,
-    "time_position" INTEGER NOT NULL,
+    "time_position" INTEGER,
     "last_contact" INTEGER NOT NULL,
-    "longitude" DOUBLE PRECISION NOT NULL,
-    "latitude" DOUBLE PRECISION NOT NULL,
-    "baro_altitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION,
+    "latitude" DOUBLE PRECISION,
+    "baro_altitude" DOUBLE PRECISION,
     "on_ground" BOOLEAN NOT NULL,
-    "velocity" DOUBLE PRECISION NOT NULL,
-    "true_track" DOUBLE PRECISION NOT NULL,
-    "vertical_rate" DOUBLE PRECISION NOT NULL,
-    "sensors" INTEGER[],
-    "geo_altitude" DOUBLE PRECISION NOT NULL,
-    "squawk" TEXT NOT NULL,
+    "velocity" DOUBLE PRECISION,
+    "true_track" DOUBLE PRECISION,
+    "vertical_rate" DOUBLE PRECISION,
+    "geo_altitude" DOUBLE PRECISION,
+    "squawk" TEXT,
     "spi" BOOLEAN NOT NULL,
     "position_source" INTEGER NOT NULL,
 
@@ -25,10 +24,10 @@ CREATE TABLE "Aircraft" (
 -- CreateTable
 CREATE TABLE "Threat" (
     "id" SERIAL NOT NULL,
-    "lat_threat" INTEGER NOT NULL,
-    "long_threat" INTEGER NOT NULL,
-    "speed_threat" INTEGER NOT NULL,
-    "range_threat" INTEGER NOT NULL,
+    "lat_threat" DOUBLE PRECISION NOT NULL,
+    "long_threat" DOUBLE PRECISION NOT NULL,
+    "speed_threat" DOUBLE PRECISION NOT NULL,
+    "range_threat" DOUBLE PRECISION NOT NULL,
 
     CONSTRAINT "Threat_pkey" PRIMARY KEY ("id")
 );
@@ -37,7 +36,7 @@ CREATE TABLE "Threat" (
 CREATE TABLE "Operation" (
     "id" SERIAL NOT NULL,
     "threatId" INTEGER NOT NULL,
-    "aircraftId" INTEGER NOT NULL,
+    "aircraftId" INTEGER,
 
     CONSTRAINT "Operation_pkey" PRIMARY KEY ("id")
 );
@@ -46,4 +45,4 @@ CREATE TABLE "Operation" (
 ALTER TABLE "Operation" ADD CONSTRAINT "Operation_threatId_fkey" FOREIGN KEY ("threatId") REFERENCES "Threat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Operation" ADD CONSTRAINT "Operation_aircraftId_fkey" FOREIGN KEY ("aircraftId") REFERENCES "Aircraft"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Operation" ADD CONSTRAINT "Operation_aircraftId_fkey" FOREIGN KEY ("aircraftId") REFERENCES "Aircraft"("id") ON DELETE SET NULL ON UPDATE CASCADE;
